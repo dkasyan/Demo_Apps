949dd4972d61129d6a60e6de08abd7ee4bdc712d
Damian Kasyan <damian.kasyan@hotmail.com>
2020-08-14 10:50:57 +0200

    Add empty file

---
+++ Exercise.py
@@ -0,0 +1,14 @@
+shoping_list = {"jajo", "jako", "jajo", 2, "jajo"}
+#print(shoping_list)
+#print(type(shoping_list))
+
+tasty_cocktail = [1, "mydło", None, "powidło", 0.1]
+#print(tasty_cocktail)
+
+
+list_of_courses = ["wschod", "zachód"]
+new_list_of_courses = list_of_courses + ["północ"]
+#print(list_of_courses)
+print(new_list_of_courses)
+list_of_courses.append("północ")
+print(list_of_courses)
