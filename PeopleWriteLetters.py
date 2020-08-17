accaec9887f9b8f7ce0973d852c2c2c48effe969
Damian Kasyan <damian.kasyan@hotmail.com>
2020-08-17 16:53:25 +0200

    Kosmita

--- PeopleWriteLetters.py
+++ PeopleWriteLetters.py
@@ -2,8 +2,6 @@ name_list = ("John", "Michael", "Terry", "Eric", "Graham")
 
 name_dictionary = dict()
 
-
-
 for name in name_list:
     lenght = (len(name))
  #   name_dictionary[name] = lenght
@@ -17,4 +15,20 @@ number_list = {1, 2, 3, 5, 6, 11, 12, 18, 19, 21}
 print(number_list)
 new_number_list = number_list.intersection(leng)
 print(new_number_list)
+
+#### Zadanie trzecie ###
+
+weak_days = {'pon','śro','pią','sob'}
+print(f"przed zmianami zbiór to {weak_days}")
+weak_days.update({"wto", "czwa", "nied"})
+print(f"po zmianach zbior to {weak_days}")
+
+#### Zadanie czwarte ###
+
+shopping_list = ["włącz czajnik", "znajdź opakowanie herbaty", "zalej herbatę", "nalej wody do czajnika", "wyjmij kubek", "włóż herbatę do kubka"]
+
+for i in range(len(shopping_list)):
+    print(shopping_list[i])
+
+
 
