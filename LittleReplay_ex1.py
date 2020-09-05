29723f741627d4b1f1b1a701a24514333e073353
Damian Kasyan <damian.kasyan@hotmail.com>
2020-09-05 23:26:51 +0200

    Add print shop

--- LittleReplay_ex1.py
+++ LittleReplay_ex1.py
@@ -1,6 +1,9 @@
 dic_shop = {
     "Papierniczy": ("ołówek", "kartki", "długopis"),
     "Warzywniak": ("pomidor", "ogórek", "ziemniaki"),
-    "Mięsny": ("Schabowy", "Mielonka")
+    "Mięsny": ("schabowy", "mielonka")
 }
 print(dic_shop["Papierniczy"])
+
+for i in dic_shop:
+    print(f"Idę do {i}, kupuję tu następujące rzeczy: {list(dic_shop[i])}")
