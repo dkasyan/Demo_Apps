2a79d66fb137c6c7430cca3fd7a07e19fa8619c3
Damian Kasyan <damian.kasyan@hotmail.com>
2020-09-05 14:07:51 +0200

    FoldingLetters.py

--- FoldingLetters.py
+++ FoldingLetters.py
@@ -14,19 +14,36 @@ print(new_list)
 #Dana jest lista: [2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 3, 0, 0]. Zadeklaruj ją w Pythonie, a następnie użyj slicingu, by otrzymać listę, która zawiera tylko zera z tej kolekcji. Potem użyj tej samej techniki do zwrócenia listy, która zawiera wszystkie inne liczby tylko nie zera z tej kolekcji.
 slice_list = [2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 3, 0, 0]
 
-new_slice_zero = []
-new_slice_number = []
+slice_zero = []
+slice_number = []
 
-new_slice_zero = slice_list[1:4] + slice_list[5:10] + slice_list[-2:]
+slice_zero = slice_list[1:4] + slice_list[5:10] + slice_list[-2:]
 
+print(slice_zero)
+new_slice_number = [ slice_number.append(element) for element in slice_list if element != 0 ]
+print(slice_number)
+
+
+
+
+### Metoda difference jest nieskuteczna, bo nie zwraca powtarzajacych się elementów listy
+
+#new_slice_list = set(slice_list)
+#new_slice_zero = set(slice_zero)
+
+
+
+#print(new_slice_list)
+#new_slice_number = new_slice_list.difference(new_slice_zero)
 #new_slice_zero.append(slice_list[1:4])
 #new_slice_zero.append(slice_list[5:10])
 #new_slice_zero.append(slice_list[-2:])
+#list(set(listone + listtwo))
+#print(new_slice_zero)
 
-print(new_slice_zero)
-new_slice_number = slice_list[0:0]
-
-
+# new_slice_number = list(slice_list[0]) + list(slice_list[4]) 
+#new_slice_number.append(slice_list[0])
 
 #new_slice_number.append(slice_list.remove())
-print(new_slice_number)
+#print(new_slice_number)
+#print(type(st1))
