1493444504545da289cda5d19375c78d19b297b3
Damian Kasyan <damian.kasyan@hotmail.com>
2020-09-05 23:21:14 +0200

    Add dictionary

---
+++ LittleReplay_ex1.py
@@ -0,0 +1,6 @@
+dic_shop = {
+    "Papierniczy": ("ołówek", "kartki", "długopis"),
+    "Warzywniak": ("pomidor", "ogórek", "ziemniaki"),
+    "Mięsny": ("Schabowy", "Mielonka")
+}
+print(dic_shop["Papierniczy"])
