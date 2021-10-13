70fd1e40c0bb1c5a4da3aac29ef73732c886f6dd
Damian Kasyan <damian.kasyan@hotmail.com>
2021-10-13 12:57:52 +0200

    Code crafting

--- BandGenerator.py
+++ BandGenerator.py
@@ -1,7 +1,8 @@
 def ask_questions():
     answer_list = []
-    x = input("Your City:\n")
-    y = input("Your pet:\n")
+    print("Welcome to the Band Name Generator.")
+    x = input("What's name of the city you grew up in?\n: ")
+    y = input("What is the name of a pet?\n: ")
     answer_list.append(x)
     answer_list.append(y)
     return answer_list
@@ -9,12 +10,11 @@ def ask_questions():
 def band_gen_funct(answer_list):
     x = answer_list[0]
     y = answer_list[1]
-    print(x + " "+ y)
+
+    print("Your band name could be " + x + " "+ y)
 
     
 
 answer_list = ask_questions()
 
 band_gen_funct(answer_list)
-
-#band_gen_funct()
