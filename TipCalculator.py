c25136aaf440d126e6b9ac9d3e5d3e2b6ecdb2f7
Damian Kasyan <damian.kasyan@hotmail.com>
2021-12-09 11:35:20 +0000

    Add questions

---
+++ TipCalculator.py
@@ -0,0 +1,4 @@
+ print("Welcome to Tip Calculator")
+ price = ("What was the total bill?\n")
+ percent = input("What percentage tip would you like to give?  ")
+ people_number = input("How many people eats?\n: ")
