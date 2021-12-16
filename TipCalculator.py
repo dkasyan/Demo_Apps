e0d646d9d0922a46773bb6c1bedea5911c6ce4bc
Damian Kasyan <damian.kasyan@hotmail.com>
2021-12-16 15:05:09 +0000

    New

--- TipCalculator.py
+++ TipCalculator.py
@@ -1,4 +1,36 @@
- print("Welcome to Tip Calculator")
- price = ("What was the total bill?\n")
- percent = input("What percentage tip would you like to give?  ")
- people_number = input("How many people eats?\n: ")
+def checking_percent(percent)
+    marker = False
+    while marker == True:
+        if percent == 10
+            print(10)
+            marker == True
+        elif percent == 12
+            print(12)
+            marker == True
+        elif percent == 15
+            print(15)
+            marker == True
+        elif
+            print("Wpisano niewlasciwy procent")
+        
+
+
+def dividing (price, percent, people_number):
+    cost = (int(price) / int(people_number)
+    print(cost)
+
+
+
+print("Welcome to Tip Calculator")
+price = input("What was the total bill?\n")
+percent = input("What percentage tip would you like to give? 10, 12, or 15 \n")
+people_number = input("How many people to split the bill?\n")
+wynik = "Each person should pay:"
+user_table = ()
+ #package = input("yes/no")
+
+
+
+
+
+dividing(price, percent, people_number)
