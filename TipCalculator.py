cb0fe9a0caf5cec95002e755ce955204fc410127
Damian Kasyan <damian.kasyan@hotmail.com>
2021-12-17 12:29:36 +0100

    Fix TipCalculator

--- TipCalculator.py
+++ TipCalculator.py
@@ -1,36 +1,14 @@
-def checking_percent(percent)
-    marker = False
-    while marker == True:
-        if percent == 10
-            print(10)
-            marker == True
-        elif percent == 12
-            print(12)
-            marker == True
-        elif percent == 15
-            print(15)
-            marker == True
-        elif
-            print("Wpisano niewlasciwy procent")
-        
-
-
-def dividing (price, percent, people_number):
-    cost = (int(price) / int(people_number)
-    print(cost)
-
+def dividing(price, percent, people_number):
+    tip_as_percent = percent / 100
+    total_tip_ammount = price * tip_as_percent
+    total_bill = price + total_tip_ammount
+    bill_per_preson = total_bill / people_number
+    print(f"Each person should pay:{bill_per_preson} zł")
 
 
 print("Welcome to Tip Calculator")
-price = input("What was the total bill?\n")
-percent = input("What percentage tip would you like to give? 10, 12, or 15 \n")
-people_number = input("How many people to split the bill?\n")
-wynik = "Each person should pay:"
-user_table = ()
- #package = input("yes/no")
-
-
-
-
+price = int(input("What was the total bill?\n"))
+percent = int(input("What percentage tip would you like to give? 10, 12, or 15 \n"))
+people_number = int(input("How many people to split the bill?\n"))
 
 dividing(price, percent, people_number)
