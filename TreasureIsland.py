091fe1c4cff461ffeaae3449068eef9b353cedde
Damian Kasyan <damian.kasyan@hotmail.com>
2021-12-30 13:00:27 +0100

    new

--- TreasureIsland.py
+++ TreasureIsland.py
@@ -1,1 +1,31 @@
-print("Welcome to Treasure Island. \n Your mission is to find the treasure! \n Chose ")
+print("Welcome to Treasure Island. \n Your mission is to find the treasure! \n Chose... left or right")
+
+def ending():
+    print("Game Over")
+    exit()
+
+k = True
+while k == True: 
+    a = input(": ")
+    if a == "left" or a == "right":
+        k = False
+    else:
+        print("Powtórz")
+
+if a == "right":
+    print("Game Over")
+    exit()
+    
+
+while k == True:
+    print("Widzisz fose co robisz? ")
+    b = input("Chose swim or wait: ")
+    if a == "swim" or a == "wait":
+        k == False
+    else:
+        print("Powtórz")
+
+#if b == "swim":
+#    ending()
+
+
