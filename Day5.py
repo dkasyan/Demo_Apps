b7b38505ac3a824fb57f403d9059b9b18af17382
Damian Kasyan <Damian.Kasyan@hotmail.com>
2022-07-13 09:22:23 +0200

    Day5

--- Day5.py
+++ Day5.py
@@ -1,10 +1,13 @@
 x = range(101)
 even_list = []
-no_even_list = [] 
+no_even_list = []
+total = 0 
 for n in x:
   k = (n % 2)
   if k == 0:
     even_list.append(n)
   else:
     no_even_list.append(n)
-print(even_list)
+for i in even_list:
+    total = i + total
+print(total)
