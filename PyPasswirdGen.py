b83d67c3c6ef9f673d29ff172356f03f3b24d8cf
Damian Kasyan <Damian.Kasyan@hotmail.com>
2022-07-15 18:41:06 +0200

    New

--- PyPasswirdGen.py
+++ PyPasswirdGen.py
@@ -15,3 +15,24 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n")) 
 
 nr_numbers = int(input(f"How many numbers would you like?\n")) 
+
+password = []
+k = 0
+
+while k != nr_letters:
+    password.append(random.choice(letters))
+    k = k + 1
+
+while k != nr_symbols:
+    password.append(random.choice(symbols))
+    k = k + 1
+
+while k != nr_numbers:
+    password.append(random.choice(numbers))
+    k = k + 1
+
+
+
+
+
+print(f"Your password:{password}")
