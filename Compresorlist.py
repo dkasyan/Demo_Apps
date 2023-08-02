e2f93d67a25e53a74f8d397b2aa41266ad3c8521
Damian Kasyan <damian.kasyan@outlook.com>
2023-08-02 11:19:51 +0200

    New

--- Compresorlist.py
+++
@@ -1,4 +0,0 @@
-numbers = [1, 2, 3,4, 5]
-element = [3, 4, 5, 6, 7]
-add_lista = [number + number for number in numbers]
-print(add_lista)
--- ShoppingList.py
+++
@@ -1,8 +0,0 @@
-a = 1
-
-def scope_demo():
-    a = 2
-    print(a)
-
-scope_demo()
-print(a)
