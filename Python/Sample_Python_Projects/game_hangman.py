4239926bd5943e428be3dfae89c9c9602ce6723a
Damian Kasyan <damian.kasyan@hotmail.com>
2023-10-08 23:26:28 +0200

    Not

--- Python/Sample_Python_Projects/game_hangman.py
+++ Python/Sample_Python_Projects/game_hangman.py
@@ -4,15 +4,24 @@ random_words = ["noga", "car", "epileptyka"]
 empty_list = []
 special_word = random.choice(random_words)
 
-guess = input("Guess a letter: ").lower()
 
-for i in special_word:
-    empty_list.append("_")
-    if guess == i:
-        print("OK")
-    else:
-        print("Wrong")
-print(empty_list)
+
+for word in range(len(special_word)):
+    print(len(special_word))
+    guess = input("Guess a letter: ").lower()
+    for i in special_word:
+        if guess == i:
+            empty_list.append(i)
+        else:
+            empty_list.append("_")
+    print(empty_list)
+
+
+
+
+
+
+
 
 #print(f"Is this what you just said? {choice}")
 
